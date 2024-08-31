6925d5f48aebef608843ea1d29f5d4db9b60f6de
preethi024 <89218062+preethi024@users.noreply.github.com>
2024-08-31 14:29:48 +0530

    Add files via upload

---
+++ Emp-System/src/main/java/com/example/demo/EmpSystemApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+@SpringBootApplication
+public class EmpSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpSystemApplication.class, args);
+	}
+}
